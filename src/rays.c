cf82c79388080bb3a396e6626956e0b3821155dc
Ignacio <itonoli42@gmail.com>
2017-09-06 01:20:02 +0200

    small change

--- src/rays.c
+++ src/rays.c
@@ -44,47 +44,6 @@ void	sphere_distance(t_env *e, t_vect ray, t_obj *obj)
 		obj->dist = -1;
 }
 
-float		dot_product(t_vect a, t_vect b)
-{
-	return (a.x * b.x + a.y * b.y + a.z * b.z);
-}
-
-t_vect		add(t_vect a, t_vect b)
-{
-	t_vect v;
-
-	v = init_vect(a.x + b.x, a.y + b.y, a.z + b.z);
-	return (v);
-}
-
-t_vect		sub(t_vect a, t_vect b)
-{
-	t_vect	v;
-
-	v = init_vect(a.x - b.x, a.y - b.y, a.z - b.z);
-	return (v);
-}
-
-t_vect	mult(t_vect a, float t)
-{
-	t_vect	v;
-
-	v.x = t * a.x;
-	v.y = t * a.y;
-	v.z = t * a.z;
-	return (v);
-}
-
-t_vect	power_v(t_vect a, int n)
-{
-	t_vect	v;
-
-	v.x = pow(a.x, n);
-	v.y = pow(a.y, n);
-	v.z = pow(a.z, n);
-	return (v);
-}
-
 /*
  * Now if need to build a function that will return 1 if the ray intersects an
  * object or 0 if not then thanks to that we will clor or black pixel
@@ -93,5 +52,5 @@ t_vect	power_v(t_vect a, int n)
 int		intersect(t_env *e)
 {
 	// if dist = -1 on ne fait rien else on colorie
-	
+
 }
