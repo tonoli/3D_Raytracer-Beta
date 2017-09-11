296128037b6d7a5c0afcb5307ceac6f795faf031
Ignacio <itonoli42@gmail.com>
2017-09-11 23:55:59 +0200

    drw

--- src/draw.c
+++ src/draw.c
@@ -9,8 +9,8 @@ void	fill_img(t_env *e)
     e->y = -1;
     while (++e->y < WIN_H)
     {
-    color = compute_objects(e, ray);
-    e->img_data[e->y][e->x] = color;
+    if (intersect(e, ray))
+      e->img_data[e->y][e->x] = WHITE;
     }
 	}
 	mlx_put_image_to_window(e->mlx, e->win, e->img, 0, 0);
